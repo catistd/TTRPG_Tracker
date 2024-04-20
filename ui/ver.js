a5bbc9a1ce06e0b7b7ddba0003412aff8858a6c7
ciander <ciander@proton.me>
2024-04-19 20:10:14 -0700

    side bar, footer + header rework

---
+++ ui/ver.js
@@ -0,0 +1,5 @@
+const { getVersion } = window.__TAURI__.app;
+const appVersion = await getVersion();
+
+document.getElementById("ver").textContent = "ver: " + String(appVersion);
+console.log(appVersion);
